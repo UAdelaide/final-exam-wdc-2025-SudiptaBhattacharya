18addc8bc745164e4d815b3a229106286df6ebf9
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 08:17:56 +0000

    [automatic commit] 2025-06-20 08:17:56 UTC 129.127.202.12

--- part1/db.js
+++ part1/db.js
@@ -7,7 +7,6 @@ let connection;
 (async () => {
     try {
 
-
         const connection = await mysql.getConnection();
         console.log('Successfully connected to the database: DogWalkService');
         connection.
