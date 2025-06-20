dcdeeb8f678ac0404d55e341f50f767e32929fa4
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 08:58:31 +0000

    [automatic commit] 2025-06-20 08:58:30 UTC 129.127.202.12

--- part1/db.js
+++ part1/db.js
@@ -4,12 +4,10 @@ let connection;
 
 const pool = mysql.createPool({
 
-        //connect to mysql without specifying a database
-         connection = await mysql.createConnection({
+
             host: 'localhost',
             user: 'root',
             password: '',
             database: 'DogWalkService'
         });
         console.log('Connected to MySQL database!');
-    
