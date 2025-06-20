6b62a0d7de6ba5882f1afe44255bf27048994ae9
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 07:54:10 +0000

    [automatic commit] 2025-06-20 07:54:10 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -5,7 +5,9 @@ var db = require('./db')
 // GET /api/dogs: returning the list of all dogs
 //with their respective name,size and owner's username
 router.get('/api/dogs', async(req, res) => {
-    const [rows] = await db.query
+    const [rows] = await db.query(
+        `SELECT `
+    )
 })
 
 module.exports = router;
