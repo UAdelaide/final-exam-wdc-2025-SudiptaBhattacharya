1d2b83526031c709e2a880c9c93ac75bbceb24d2
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:49:23 +0000

    [automatic commit] 2025-06-20 09:49:22 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -57,5 +57,8 @@ router.get('/walkrequests/open', async(req, res) => {
 // no. of completed walks
 
 //there is no average_rating and total_rating, instead we can keep count of the ratings
+//as total_ratings and round them to get average_rating
+
+router.get('/')
 
 module.exports = router;
