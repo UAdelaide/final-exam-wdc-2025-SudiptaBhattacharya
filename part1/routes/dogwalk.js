6ae673b47b96b937fb440b449fb0e190ef24fa06
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:53:32 +0000

    [automatic commit] 2025-06-20 09:53:32 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -57,8 +57,9 @@ router.get('/walkrequests/open', async(req, res) => {
 // no. of completed walks
 
 //there is no average_rating and total_rating, instead we can keep count of the rating_id
-//count(rating_id) will count how many times a walker was rated by o
-//as total_ratings and round them to get average_rating
+//count(rating_id) will count how many times a walker was rated by owner
+//as total_ratings and
+//round them to get average_rating
 
 router.get('/walkers/summary', async(req, res) => {
     try{
