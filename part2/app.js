d258cdbc6bbc9c5fd28494a81da8a29e62dc6a43
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 15:41:22 +0000

    [automatic commit] 2025-06-20 15:41:22 UTC 129.127.202.12

--- part2/app.js
+++ part2/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+//add express-session route for session management:
+const session = require()
 
 const app = express();
 
