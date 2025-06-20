9a898a64b4e675897e1fb5ff1ae596ddd3305d77
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 16:45:14 +0000

    [automatic commit] 2025-06-20 16:45:14 UTC 129.127.202.12

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -174,32 +174,34 @@ function downvote(index) {
 }
 
 
-function login(){
+// ... (previous functions remain unchanged)
 
+async function loginQASystem() {
     let user = {
         user: document.getElementById('username').value,
         pass: document.getElementById('password').value
     };
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+    try {
+        const response = await fetch('/users/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user)
+        });
 
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
+        if (response.ok) {
+            const username = await response.text();
+            alert("Welcome " + username);
+        } else {
+            throw new Error('Login failed');
         }
-    };
-
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
-
+    } catch (error) {
+        alert("Login failed");
+        console.error('Login error:', error);
+    }
 }
+
+// ... (rest of the file remains unchanged)
 
 async function logout() {
     try {
