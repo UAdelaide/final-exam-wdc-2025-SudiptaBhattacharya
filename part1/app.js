b9b3d152db693e35426b2cf8c75109309248a0b4
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:05:45 +0000

    [automatic commit] 2025-06-20 09:05:45 UTC 129.127.202.12

--- part1/app.js
+++ part1/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 //add new route path for dogs and dogs walk requests
-var dogWalkROuter = require('./routes/dogwalk');
+var dogWalkRouter = require('./routes/dogwalk');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //mount dogs and requests route
-app.use('/api', dogWalkROuter);
+app.use('/api', dogWalkRouter);
 
 const insertSampleData = async () => {
     try {
