c704f24dc8bfa6d0dd08cd2eeca6960f503a36e9
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:50:25 +0000

    [automatic commit] 2025-06-20 09:50:25 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -62,7 +62,7 @@ router.get('/walkrequests/open', async(req, res) => {
 router.get('/walkers/summary', async(req, res) => {
     try{
         const [rows] = await db.query(`
-        
+        SELECT u.username AS walker_username,
         `)
     }
 })
