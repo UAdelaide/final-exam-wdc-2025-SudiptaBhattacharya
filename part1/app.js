8387fc17ca5a8d343a696e5eb679d71ded499d08
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:26:53 +0000

    [automatic commit] 2025-06-20 09:26:53 UTC 129.127.202.12

--- part1/app.js
+++ part1/app.js
@@ -54,7 +54,8 @@ const insertSampleData = async () => {
 
       const [walkRequestRows] = await db.query('SELECT COUNT(*) AS count FROM WalkRequests');
           if (walkRequestRows[0].count == 0) {
-        INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+            await db.query(`
+            INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
         VALUES
           (1, '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
           (2, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
@@ -62,7 +63,8 @@ const insertSampleData = async () => {
           (5, '2025-06-12 14:00:00', 60, 'BedFord Park', 'cancelled'),
           (4, '2025-06-13 16:00:00', 20, 'HindMarsh Square', 'open');
       `);
-          
+      
+
 
       console.log('Sample data inserted.');
     } catch (err) {
