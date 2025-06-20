8fb48febf1a0adda985bcfc9bfb725f29d31ad30
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 08:59:53 +0000

    [automatic commit] 2025-06-20 08:59:53 UTC 129.127.202.12

--- part1/app.js
+++ part1/app.js
@@ -24,4 +24,42 @@ app.use('/users', usersRouter);
 //mount dogs and requests route
 app.use('/api', dogWalkROuter);
 
+const insertSampleData = async () => {
+    try {
+      await db.query(`
+        INSERT IGNORE INTO Users (username, email, password_hash, role)
+        VALUES
+          ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+          ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+          ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+          ('reet120', 'reet@example.com', 'hashed200', 'walker'),
+          ('alex999', 'alex@example.com', 'hashed550', 'walker');
+      `);
+
+      await db.query(`
+        INSERT IGNORE INTO Dogs (owner_id, name, size)
+        VALUES
+          (1, 'Max', 'medium'),
+          (3, 'Bella', 'small'),
+          (1, 'Mylo', 'small'),
+          (3, 'Teddy', 'large'),
+          (3, 'Becky', 'medium');
+      `);
+
+      await db.query(`
+        INSERT IGNORE INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+        VALUES
+          (1, '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+          (2, '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+          (3, '2025-06-11 10:00:00', 25, 'Botanic Park', 'completed'),
+          (5, '2025-06-12 14:00:00', 60, 'BedFord Park', 'cancelled'),
+          (4, '2025-06-13 16:00:00', 20, 'HindMarsh Square', 'open');
+      `);
+
+      console.log('Sample data inserted.');
+    } catch (err) {
+      console.error('Error inserting sample data:', err);
+    }
+  };
+
 module.exports = app;
