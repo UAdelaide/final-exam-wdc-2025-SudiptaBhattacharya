64d0d19623f2e49fba53e27a28521080616b6a08
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:14:03 +0000

    [automatic commit] 2025-06-20 09:14:03 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -8,11 +8,11 @@ router.get('/dogs', async(req, res) => {
     //use try-catch block for error check
     try{
     const [rows] = await db.query(
-        `SELECT d.name AS dog, d.size,
-            u.username
+        `SELECT d.name AS dog_name, d.size,
+            u.username AS owner_username
         FROM Dogs d
         JOIN Users u ON d.owner_id = u.user_id
-        ORDER BY d.name;
+        
         `
     );
     res.json(rows);
