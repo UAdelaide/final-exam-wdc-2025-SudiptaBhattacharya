24b897ad369404c18b14251cbfeb65fd4c86690e
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-20 09:57:19 +0000

    [automatic commit] 2025-06-20 09:57:19 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -79,7 +79,8 @@ router.get('/walkers/summary', async(req, res) => {
     catch(error){
         console.error("Error in returning summary of each walker ", error);
         res.status(500).json({
-            error: 'Failed to fetch the summary of '});
+            error: `Failed to fetch the summary of each walker with their average
+            rating`});
 
     }
 });
