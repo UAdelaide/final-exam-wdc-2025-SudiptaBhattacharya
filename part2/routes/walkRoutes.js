c8637e1b21b65078479aabc53a9fa28d71f0a589
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-21 03:59:15 +0000

    [automatic commit] 2025-06-21 03:59:15 UTC 129.127.202.12

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../models/db');
 
 //handle /api/dogs
+//modifying the /api/dogs 
 router.get('/dogs', async (req, res) => {
   try {
     const ownerId = req.query.owner_id;
