92c64368e4e37d7bbcf2b50cbb4de71b8f4a4fe7
Sudipta Bhattacharya <a1919390@adelaide.edu.au>
2025-06-21 07:03:06 +0000

    [automatic commit] 2025-06-21 07:03:06 UTC 129.127.202.12

--- part1/routes/dogwalk.js
+++ part1/routes/dogwalk.js
@@ -21,7 +21,7 @@ router.get('/dogs', async(req, res) => {
     catch(error){
         console.error("Error fetching the list of dogs owned by each owner: ", error);
         //send status code in page and error message
-        //return 
+        //return 500 Internal Server error with a json message in that path
         res.status(500).json({
             message: 'Failed to get list of dogs', error: error.message})
         }
